3d198aa5a5be717168c6e020c3bdc73fc56fb41f
Osman Mamed <osmanmamed@gmail.com>
2025-09-19 19:03:21 -0500

    feat: get e set criado para a classe usuário

--- aula-java/aulajava/src/Usuario.java
+++ aula-java/aulajava/src/Usuario.java
@@ -6,3 +6,33 @@ public class Usuario {
     private Boolean ativo;
     private String papel; 
 }
+public long getId() {
+    return id;
+}
+public void setId(long id) {
+    this.id = id;
+}
+public String getnomeCompleto() {
+    return nomeCompleto;
+}
+public void setnomeCompleto(String nomeCompleto){
+    this.nomeCompleto = nomeCompleto;
+}
+public String getnomeUsuario(){
+    return nomeUsuario;
+}
+public void setnomeUsuario(String nomeUsuario){
+    this.nomeUsuario = nomeUsuario;
+}
+public Boolean getAtivo(){
+    return ativo;
+}
+public void setAtivo(Boolean ativo){
+    this.ativo = ativo;
+}
+public String getPapel(){
+    return papel;
+}
+public void setPapel(String papel){
+    this.papel = papel;
+}
